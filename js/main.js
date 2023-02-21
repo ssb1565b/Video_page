5891e2949d79377b5fb4802f3cb1caaf39fbea5a
ssb1565b <ssongsumovie@naver.com>
2023-02-21 14:30:47 +0900

    cursor

--- js/main.js
+++ js/main.js
@@ -8,3 +8,17 @@ snbBtn.addEventListener("click", function () {
   }
   subMenu.classList.add("hide");
 });
+
+const cursor = document.getElementById("circle");
+let posX = 0;
+let posY = 0;
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.body.onmousemove = function (e) {
+    posX = e.clientX + "px";
+    posY = e.clientY + "px";
+
+    cursor.style.left = posX;
+    cursor.style.top = posY;
+  };
+});
